d5442806307144f2dfe715faff8900f8e2b66f02
AlmightyBryan <bryan19251@gmail.com>
2024-08-28 16:04:00 -0600

    servicios
    opcion de servicios

--- servicios.php
+++ servicios.php
@@ -1,30 +1,68 @@
 <?php
-$title = "Adopciones - Clínica Veterinaria";
-include 'header.php';
+include("functions.php");
+$menu = getMenu();
 ?>
+<!DOCTYPE html>
+<html>
 
-        <section>
-            <h2>Nuestros Servicios</h2>
-            <p>En nuestra clínica veterinaria ofrecemos una amplia gama de servicios para mantener a tu mascota saludable y feliz:</p>
+<head>
+    <meta charset="UTF-8">
+    <title>Servicios - Veterinaria San Mateo</title>
+    <link rel="stylesheet" href="./css/style.css">
+    <script src="./js/jquery-3.7.1.js"></script>
+    <script src="./js/scriptvjquery.js"></script>
+</head>
+
+<body>
+    <header>
+        <h1>Veterinaria San Mateo</h1>
+        <nav>
             <ul>
-                <li>Consultas generales y de especialidad</li>
-                <br>
-                <li>Vacunación</li>
-                <br>
-                <li>Desparasitación</li>
-                <br>
-                <li>Cirugías</li>
-                <br>
-                <li>Radiografías y ecografías</li>
-                <br>
-                <li>Hospitalización</li>
-                <br>
-                <li>Laboratorio clínico</li>
-                <br>
-                <li>Grooming</li>
-                <br>
-                <li>Venta de alimentos y accesorios</li>
+                <?php foreach ($menu as $item) { ?>
+                    <li><a href="<?php echo $item["url"] ?>"><?php echo $item["name"] ?></a></li>
+                <?php } ?>
             </ul>
+        </nav>
+    </header>
+    <main>
+        <section>
+            <h2>Servicios</h2>
+            <p>Le invitamos a iniciar sesión para acceder a nuestos servicios.</p>
+            <p style="text-align: left;">Ofrecemos para su mascota:</p>
+            <ul class="service-list">
+             <li>Consultas generales y de especialidad</li>
+             <li>Vacunación</li>
+             <li>Desparasitación</li>
+             <li>Cirugías</li>
+             <li>Radiografías y ecografías</li>
+             <li>Hospitalización</li>
+             <li>Laboratorio clínico</li>
+             <li>Grooming</li>
+             <li>Venta de alimentos y accesorios</li>
+            </ul>
+            <div class="service-container">
+                <div class="service-box">
+                    <img src="./images/corte.jpg" alt="Servicio 1">
+                    <h3>Corte de cabello</h3>
+                    <a href="ingresar.php" class="button">Iniciar Sesión</a>
+                </div>
+                <div class="service-box">
+                    <img width="215" height="90"src="./images/consejos.jpg" alt="Servicio 2">
+                    <h3>Consejos y recomendaciones</h3>
+                    <a href="ingresar.php" class="button">Iniciar Sesión</a>
+                </div>
+                <div class="service-box">
+                    <img src="./images/medicamentos.jpg" alt="Servicio 3">
+                    <h3>Medicamentos</h3>
+                    <a href="ingresar.php" class="button">Iniciar Sesión</a>
+                </div>
+            </div>
         </section>
-        
-<?php include 'footer.php'; ?>
+    </main>
+
+    <footer>
+        <p>&copy; 2024 Veterinaria San Mateo. Todos los derechos reservados.</p>
+    </footer>
+</body>
+
+</html>
